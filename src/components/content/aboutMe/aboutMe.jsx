b43eb353107c64630d84a4e703b630849e4a9130
Palyka Dima <polykodima@mail.ru>
2021-04-11 12:26:16 +0300

    feat: start about me

--- src/components/content/aboutMe/aboutMe.jsx
+++ src/components/content/aboutMe/aboutMe.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Class from "./aboutMe.module.css";
 import { NavLink } from 'react-router-dom';
 
+const ItemAboutMe = (props) => {
+    let path = '/about_me/' + props.id;
+    return (
+        <div className={Class.item}>
+            <NavLink to={path} activeClassName={Class.active}>{props.content}</NavLink>
+        </div>
+    );
+};
+
 const AboutMe = () => {
     return (
         <div className={Class.wrapper}>
             <div className={Class.items}>
-                <div className={Class.item}>
-                    <NavLink to='/about_me/main' activeClassName={Class.active}>main info</NavLink>
-                </div>
-                <div className={Class.item}>
-                    <NavLink to='/about_me/objective' activeClassName={Class.active}>objective</NavLink>
-                </div>
+                <ItemAboutMe id='main' content='main info'></ItemAboutMe>
+                <ItemAboutMe id='objective' content='objective'></ItemAboutMe>
             </div>
             <div className={Class.items_content}>
                 <div>
